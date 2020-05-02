9688f469fe717fd525dde34928028dd52fe09e3c
Micah Wood <micah@wpscholar.com>
2020-05-02 11:50:18 -0400

    Tweak cleanup script

--- .github/scripts/cleanup.sh
+++ .github/scripts/cleanup.sh
@@ -4,10 +4,10 @@ rm -rf .git
 rm -rf .github
 rm -rf .wporg
 rm -rf node_modules
+rm -rf vendor
 find . -name '.*' -type f -maxdepth 1 -delete
 find . -name '*.md' -type f -maxdepth 1 -delete
 find . -name '*.json' -type f -maxdepth 1 -delete
 find . -name '*.lock' -type f -maxdepth 1 -delete
 find . -name '*.xml' -type f -maxdepth 1 -delete
 find . -name '*.yml' -type f -maxdepth 1 -delete
-rm -f webpack.config.js
