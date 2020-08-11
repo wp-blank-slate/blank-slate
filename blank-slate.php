9d895ac4a938b46f56fc3a1d87619a247f3589f9
Micah Wood <micah@wpscholar.com>
2020-08-11 14:56:08 -0400

    Update plugin headers and bump to version 1.2

--- blank-slate.php
+++ blank-slate.php
@@ -1,21 +1,25 @@
 <?php
 /**
- * Plugin Name: Blank Slate
- * Description: Provides a blank page template for use with WordPress page builders.
- * Plugin URI:  https://wpblankslate.com/
- * Version:     1.1.5
- * Author:      Aaron Reimann
- * Author URI:  https://aaronreimann.com/
+ * Blank Slate
+ *
+ * @package           BlankSlate
+ * @author            Micah Wood
+ * @copyright         Copyright 2019-2020 by Aaron Reimann & Micah Wood - All rights reserved.
+ * @license           GPL2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Blank Slate
+ * Plugin URI:        https://wpblankslate.com
+ * Description:       Provides a blank page template for use with WordPress page builders.
+ * Version:           1.2
+ * Requires PHP:      5.3
  * Requires at least: 4.7
- * Requires PHP: 5.3
- * Text Domain: blank-slate
- * Domain Path: languages
- * License:     GPL2
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- *
- * Copyright 2019-2020 by Aaron Reimann & Micah Wood - All rights reserved.
- *
- * @package BlankSlate
+ * Author:            Aaron Reimann
+ * Author URI:        https://aaronreimann.com
+ * Text Domain:       blank-slate
+ * Domain Path:       /languages
+ * License:           GPL V2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 require __DIR__ . '/functions.php';
